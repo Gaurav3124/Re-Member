9c9851270c59eef5ce89414a1f3946c70769843b
Gaurav Shah <gauravshah@Gauravs-MacBook-Pro.local>
2024-09-28 12:16:25 +0530

    initial commit

---
+++ src/main/java/com/re_member/core_re_member/CoreReMemberApplication.java
@@ -0,0 +1,13 @@
+package com.re_member.core_re_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreReMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreReMemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/re_member/core_re_member/CoreReMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re_member.core_re_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreReMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
